17e3436b6959a4c67d46b5c82581d7a8334afde5
rajesh9938 <rajeshchintu2000@gmail.com>
2022-08-10 19:38:33 +0530

    submit

--- gitpush.py
+++ gitpush.py
@@ -5,3 +5,11 @@ while a>0:
     r=r*10+t
     a=a//10
 print(r)
+
+w=[23,65,2,6,82,23]
+a=len(w)
+for i in range(a):
+    for j in range(i+1,a):
+        if w[i]>w[j] :
+            w[i],w[j]=w[j],w[i]
+print(w)
